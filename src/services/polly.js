47a3f740f6cb48426dabacf0ef486750c139c42e
Souhaib LAANANI <sohaib.laanani@gmail.com>
2022-12-16 02:43:27 +0100

    Move keys to .env

--- src/services/polly.js
+++ src/services/polly.js
@@ -3,8 +3,8 @@ import { PollyClient } from "@aws-sdk/client-polly";
 const client = new PollyClient({
   region: "eu-west-3",
   credentials: {
-    accessKeyId: "AKIAWFEAMNQCKPI7HAPV",
-    secretAccessKey: "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",
+    accessKeyId: process.env.REACT_APP_AMAZON_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AMAZON_SECRET_ACCESS_KEY,
   },
 });
 
